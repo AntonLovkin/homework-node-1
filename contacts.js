2a445e372389d373b185982189c96d02e83518c3
AntonLovkin <anton28111987@gmail.com>
2021-08-27 23:18:16 +0300

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises
+const path = require('path')
+
+// Раскомментируй и запиши значение
+const contactsPath = path.join;
+
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
