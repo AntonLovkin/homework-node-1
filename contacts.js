021e67d0ee6ec479d483335ec1ebc677000c5a2d
AntonLovkin <anton28111987@gmail.com>
2021-08-28 18:47:26 +0300

    Update contacts.js
    step 3

--- contacts.js
+++ contacts.js
@@ -1,3 +1,4 @@
+const { v4 } = require('uuid');
 const fs = require('fs').promises;
 const path = require('path');
 
@@ -9,25 +10,62 @@ async function listContacts() {
     try {
         const data = await fs.readFile(contactsPath);
         const contacts = JSON.parse(data);
-        console.log(contacts);
+        // console.log(contacts);
+      return contacts;
         
     } catch (error) {
         throw error;
     }
 }
 
-function getContactById(contactId) {
-  // ...твой код
-    console.log('Hello');
+async function getContactById(contactId) {
+  try {
+      const contacts = await listContacts();
+      const selectContactById = contacts.find(item => item.id === contactId);
+        if(!selectContactById){
+            throw new Error(`Contact with id=${contactId} not found`);
+        }
+        return selectContactById;
+    
+    }
+    catch(error){
+        throw error;
+    }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+        const contacts = await listContacts();
+        const idx = contacts.findIndex(item => item.id === contactId);
+        if(idx === -1){
+            throw new Error(`Contact with id=${contactId} not found`);
+        }
+        const newListContacts = contacts.filter(item => item.id !== contactId);
+        // const delProduct = products.splice(idx, 1);
+        await updateContacts(newListContacts);
+        return contacts[idx];
+    }
+    catch(error){
+        throw error;
+    }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+try {
+  const newContact = { name, email, phone, id: v4() };
+  const contacts = await listContacts();
+  const newListContacts =  contacts.push(newContact);
+  await updateContacts(newListContacts);
+  return newContact;
 }
+catch (error) { 
+}
+};
+
+async function updateContacts (contacts) {
+    const contactsString = JSON.stringify(contacts);
+    await fs.writeFile(contactsPath, contactsString);
+};
 
 module.exports = {
     listContacts,
