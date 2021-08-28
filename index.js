bad299abd3c648ea2b3bec0da556ff9e9ffb9190
AntonLovkin <anton28111987@gmail.com>
2021-08-28 18:47:36 +0300

    Update index.js
    step 3

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 const contactsOperations = require('./contacts');
 
-const contacts =  contactsOperations.listContacts();
-console.log(contacts);
+(async () => {
+    try {
+        // const contacts = await contactsOperations.listContacts();
+        // console.log(contacts);
+        
+        // const id = 10;
+        // const contacts = await contactsOperations.getContactById(id);
+        // console.log(contacts);
+        
+        // const id = 9;
+        // const contacts = await contactsOperations.removeContact(id);
+        // console.log(contacts);
+
+        // const newContact = await contactsOperations.addContact("Tom", "fffff@mail.com", "123456");
+        // console.log(newContact);
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+})();
+// const contacts =  contactsOperations.listContacts();
+// console.log(contacts);
+
+// const contacts = contactsOperations.getContactById(1);
+// console.log(contacts);
